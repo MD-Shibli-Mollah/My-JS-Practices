59f68b394989dda5e4cd2d2e7309f541fd1ea170
MD-Shibli-Mollah <md.ta.shibli@gmail.com>
2021-06-12 00:25:06 +0600

    new spread

--- spread.js
+++ spread.js
@@ -12,7 +12,9 @@ const copPerson = {...person}
 console.log(copPerson);
 
 //Arr Portion
-
 const myCourses = ['js', 'Node.js', 'mongoDB', 'Express.js']
 let copMyCrs = [...myCourses]
 console.log(copMyCrs);
+//more ***notes: Numbers of args can be sent using ... (3 dots) and position the by their Array Pos
+const toArr = (...args) => args[0] + args[1] + args[2] + args[3] + args[4] + args[5];
+console.log(toArr(1,2,3,4,5,6));
